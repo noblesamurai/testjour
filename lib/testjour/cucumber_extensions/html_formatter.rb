ecdc613d80c993577f58956ea562a497746a996f
Rob Aldred <raldred@gmail.com>
2010-04-12 16:08:51 +0100

    kill some more functions that we dont need

--- lib/testjour/cucumber_extensions/html_formatter.rb
+++ lib/testjour/cucumber_extensions/html_formatter.rb
@@ -3,7 +3,7 @@ require 'cucumber/formatter/html'
 module Testjour
 
   class HtmlFormatter < Cucumber::Formatter::Html
-    
+
     #stop the html formatter doing JS otherwise it's almost impossible to load the page in a browser
     def after_step(step)
       # do nothing
@@ -14,6 +14,18 @@ module Testjour
     end
     
     def move_progress
+      # do nothing
+    end
+    
+    def inline_js_content
+      # do nothing
+    end
+    
+    def set_scenario_color_failed
+      # do nothing
+    end
+    
+    def set_scenario_color_pending
       # do nothing
     end
     
