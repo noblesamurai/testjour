b8f98a9fcb084532873a580d988f69e5a5f08630
Rob Aldred <rob.aldred@setfiremedia.com>
2010-05-05 10:51:50 +0100

    no need to pass on master host to child slave commands. rsync is done by that point

--- lib/testjour/configuration.rb
+++ lib/testjour/configuration.rb
@@ -206,9 +206,6 @@ module Testjour
       if @options[:queue_prefix]
         args_from_options << "--queue-prefix=#{@options[:queue_prefix]}"
       end
-      if @options[:master_host]
-        args_from_options << "--master-host=#{@options[:master_host]}"
-      end
       return args_from_options
     end
 
