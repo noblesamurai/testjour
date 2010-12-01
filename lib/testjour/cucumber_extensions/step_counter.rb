c4cfd29b292140f67806e1bdb0247c282a15a92a
Noble Samurai <samurai@swift.internal.alliancesoftware.com.au>
2010-12-01 12:38:12 +1100

    oops. background steps get listed twice... the first lot is now ignored when calculating the number of steps

--- lib/testjour/cucumber_extensions/step_counter.rb
+++ lib/testjour/cucumber_extensions/step_counter.rb
@@ -3,30 +3,34 @@ module Testjour
     class StepCounter
 
       def initialize
-	  	@step_count = 0
+        @step_count = 0
       end
 
-	  def before_feature(feature)
-	  	@feature_step_count = 0
-		@feature_row_count = nil
-	  end
+      def before_feature(feature)
+        @feature_step_count = nil
+        @feature_row_count = nil
+      end
 
-	  def after_feature(feature)
-	  	@feature_step_count *= @feature_row_count if !@feature_row_count.nil?
-		@step_count += @feature_step_count
-	  end
+      def before_feature_element(feature_element)
+        @feature_step_count = 0
+      end
+
+      def after_step(step)
+        @feature_step_count += 1 if !@feature_step_count.nil?
+      end
 
       def before_outline_table(outline_table)
-	  	@feature_row_count = 0
+        @feature_row_count = 0
       end
 
-	  def after_step(step)
-	  	@feature_step_count += 1
-	  end
+      def after_table_row(table_row)
+        @feature_row_count += 1 if table_row.scenario_outline
+      end
 
-	  def after_table_row(table_row)
-	  	@feature_row_count += 1 if table_row.scenario_outline
-	  end
+      def after_feature_element(feature_element)
+        @feature_step_count *= @feature_row_count if !@feature_row_count.nil?
+        @step_count += @feature_step_count
+      end
 
       def count
         @step_count
